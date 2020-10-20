955fc0cead2f0e5a167a4181939c75d299b68b2e
root <root@MSI.localdomain>
2020-10-20 22:10:32 +1300

    dfas

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const template = 'home'
+const art = require('./art.json')
+const viewData = {
+  title: "Gallery",
+  subheading: "List of Art",
+  greeting: "Hello, world!",
+  art: art,
+  name: art,
+  license: art,
+  header: "Awesome Art Gallery!!",
+  footer: "Lukin, Mave and Roy!"
+}
 
 const server = express()
 module.exports = server
@@ -12,3 +24,15 @@ server.set('view engine', 'hbs')
 server.use(express.static('public'))
 
 // Routes
+
+
+server.get('/', (req, res) => {
+  res.render(template, viewData)
+})
+
+
+server.get('/artwork/:id', (req, res) => {
+  const id = req.params.id
+  const item = art.find((el) => el.id == id)
+  res.render("artwork", item)
+})
